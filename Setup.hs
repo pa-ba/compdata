f62ba876b96f4be416e16464ed9473b661c37c81
Patrick Bahr <pa-ba@arcor.de>
2010-09-01 00:55:27 +0200

    added code to test setup script that removes old test.tix file

--- Setup.hs
+++ Setup.hs
@@ -3,7 +3,8 @@ import Distribution.Simple.LocalBuildInfo
 import Distribution.PackageDescription
 import System.Cmd
 import System.FilePath
-
+import System.Directory
+import System.IO.Error
 
 
 main = defaultMainWithHooks hooks
@@ -14,6 +15,11 @@ hpcReportDir = "hpcreport"
 
 runTests' :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
 runTests' _ _ _ lbi = do
+  res <- try (removeFile tixFile)
+  case res of
+    Left err
+        | not (isDoesNotExistError err) -> putStrLn "tix file could not be removed"
+    _ -> return ()
   putStrLn "running tests ..."
   system testprog
   putStrLn "computing code coverage ..."
@@ -22,6 +28,7 @@ runTests' _ _ _ lbi = do
   hpcMarkup
   return ()
     where testprog = (buildDir lbi) </> "test" </> "test"
+          tixFile = "test.tix"
           hpcReport = system $ "hpc report test"++exclArgs
           hpcMarkup = system $ "hpc markup test --destdir="++hpcReportDir++exclArgs
           excludedModules = []
