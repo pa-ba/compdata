7ac534fbc3642a146bc9f02cf476c11e0ec627ae
Patrick Bahr <pa-ba@arcor.de>
2011-01-03 20:28:12 +0100

    - change the implementation of the monadic traverse functions mapM and sequence form an applicative style to a direct monadic style (using bind instead of ap) - this gives a considerable performance gain such that the composition on the algebra level is faster than the composition on the function level for monadic algebras as well!

--- src/Data/ALaCarte/Derive/Traversable.hs
+++ src/Data/ALaCarte/Derive/Traversable.hs
@@ -17,12 +17,13 @@ module Data.ALaCarte.Derive.Traversable
 
 import Data.ALaCarte.Derive.Utils
 import Language.Haskell.TH
+import Data.Maybe
 import Data.Traversable
 import Data.Foldable hiding (or)
 import Control.Applicative
 import Control.Monad hiding (mapM, sequence)
-import qualified Prelude as P (foldl, mapM)
-import Prelude hiding  (foldl,mapM, sequence)
+import qualified Prelude as P (foldl, foldr, mapM)
+import Prelude hiding  (foldl, foldr,mapM, sequence)
 
 instanceTraversable :: Name -> Q [Dec]
 instanceTraversable fname = do
@@ -39,35 +40,40 @@ instanceTraversable fname = do
   return $ [InstanceD [] classType [traverseDecl, sequenceADecl,mapMDecl, sequenceDecl]]
       where isFarg fArg (constr, args) = (constr, map (== fArg) args)
             filterVar farg nonFarg incl x
-                | incl = farg $ varE x
-                | otherwise = nonFarg $ varE x
+                | incl = farg x
+                | otherwise = nonFarg x
             filterVars args varNs farg nonFarg = zipWith (filterVar farg nonFarg) args varNs
             mkCPat constr varNs = ConP constr $ map mkPat varNs
             mkPat x = VarP x
             mkPatAndVars (constr, args) =
                 do varNs <- newNames (length args) "x"
-                   return (conE constr, mkCPat constr varNs, filterVars args varNs, or args)
-            traverseClause (con, pat,vars',hasFargs) =
+                   return (conE constr, mkCPat constr varNs,
+                           (\ f g -> filterVars args varNs (f . varE) (g . varE)),
+                           or args, map varE varNs, catMaybes $ filterVars args varNs Just (const Nothing))
+            traverseClause (con, pat,vars',hasFargs,_,_) =
                 do fn <- newName "f"
                    let f = varE fn
                        fp = if hasFargs then VarP fn else WildP
                        vars = vars' (\x -> [|$f $x|]) (\x -> [|pure $x|])
                    body <- P.foldl (\ x y -> [|$x <*> $y|]) [|pure $con|] vars
                    return $ Clause [fp, pat] (NormalB body) []
-            sequenceAClause (con, pat,vars',hasFargs) =
-                do 
-                   let vars = vars' id (\x -> [|pure $x|])
+            sequenceAClause (con, pat,vars',hasFargs,_,_) =
+                do let vars = vars' id (\x -> [|pure $x|])
                    body <- P.foldl (\ x y -> [|$x <*> $y|]) [|pure $con|] vars
                    return $ Clause [pat] (NormalB body) []
-            mapMClause (con, pat,vars',hasFargs) =
+            -- Note: the monadic versions are not defined
+            -- applicatively, as this results in a considerable
+            -- performance penalty (by factor 2)!
+            mapMClause (con, pat,_,hasFargs,allVars, fvars) =
                 do fn <- newName "f"
                    let f = varE fn
                        fp = if hasFargs then VarP fn else WildP
-                       vars = vars' (\x -> [|$f $x|]) (\x -> [|return $x|])
-                   body <- P.foldl (\ x y -> [|$x `ap` $y|]) [|return $con|] vars
+                       conAp = P.foldl appE con allVars
+                   body <- P.foldr (\ x y -> [|$f $(varE x)  >>= $(lamE [varP x] y)|])
+                           [|return $conAp|] fvars
                    return $ Clause [fp, pat] (NormalB body) []
-            sequenceClause (con, pat,vars',hasFargs) =
-                do 
-                   let vars = vars' id (\x -> [|return $x|])
-                   body <- P.foldl (\ x y -> [|$x `ap` $y|]) [|return $con|] vars
+            sequenceClause (con, pat,_,hasFargs,allVars, fvars) =
+                do let conAp = P.foldl appE con allVars
+                   body <- P.foldr (\ x y -> [|$(varE x)  >>= $(lamE [varP x] y)|])
+                           [|return $conAp|] fvars
                    return $ Clause [pat] (NormalB body) []
