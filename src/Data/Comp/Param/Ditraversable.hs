8c243ea4d01d7e32e51f4361633c61766a6e2e8e
Patrick Bahr <pa-ba@arcor.de>
2011-04-13 14:05:40 +0100

    added ditraversable instances for reader monads (including QuickCheck's Gen monad)

--- src/Data/Comp/Param/Ditraversable.hs
+++ src/Data/Comp/Param/Ditraversable.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RankNTypes, FlexibleInstances, MultiParamTypeClasses,
-  FlexibleContexts, IncoherentInstances #-}
+  FlexibleContexts, OverlappingInstances #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Comp.Param.Ditraversable
@@ -21,6 +21,9 @@ module Data.Comp.Param.Ditraversable
 import Prelude hiding (mapM, sequence, foldr)
 import Data.Comp.Param.Difunctor
 import Data.Traversable
+import Test.QuickCheck.Gen
+import Data.Functor.Identity
+import Control.Monad.Reader hiding (mapM, sequence)
 
 {-| Difunctors representing data structures that can be traversed from left to
   right. -}
@@ -36,3 +39,19 @@ class (Difunctor f, Monad m) => Ditraversable f m a where
 instance (Difunctor f, Monad m, Traversable (f a)) => Ditraversable f m a where
     dimapM = mapM
     disequence = sequence
+
+instance Ditraversable (->) Gen a where
+    dimapM f s = MkGen run
+        where run stdGen seed a = unGen (f (s a)) stdGen seed
+
+instance Ditraversable (->) Identity a where
+    dimapM f s = Identity run
+        where run a = runIdentity (f (s a))
+
+instance Ditraversable (->) (ReaderT r Identity) a where
+    dimapM f s = ReaderT (Identity . run)
+        where run r a = runIdentity $ runReaderT (f (s a)) r
+
+instance Ditraversable (->) m a =>  Ditraversable (->) (ReaderT r m) a where
+    dimapM f s = ReaderT (disequence . run)
+        where run r a = runReaderT (f (s a)) r
