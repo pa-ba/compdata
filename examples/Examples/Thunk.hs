e476cf34686a322c02f09ade920653fa60c48d99
Patrick Bahr <pa-ba@arcor.de>
2011-11-01 13:05:27 +0100

    implemented two variants of haskellStrict that differ slightly in their strictness (in practice both are equivalent and the primed version provides better performance as it is slightly more strict)

--- examples/Examples/Thunk.hs
+++ examples/Examples/Thunk.hs
@@ -25,7 +25,7 @@ import Data.Comp.Show()
 import Control.Monad
 
 -- Signature for values and operators
-data Value e = Const Int | Pair e !e
+data Value e = Const Int | Pair !e !e
 data Op e = Add e e | Mult e e | Fst e | Snd e
 
 -- Signature for the simple expression language
@@ -58,7 +58,7 @@ instance (Value :<: v) => EvalT Value v where
 --  evalAlgT x = inject x
 
 -- or only partially strict
-  evalAlgT = haskellStrict
+  evalAlgT = haskellStrict'
 
 instance (Value :<: v) => EvalT Op v where
   evalAlgT (Add x y) = thunk $ do
@@ -86,13 +86,3 @@ instance Monad (Either String) where
 
 evalTEx :: Either String (Term Value)
 evalTEx = evalT (iSnd (iFst (iConst 5) `iPair` iConst 4) :: Term Sig)
-
-
-
-data Bar a = F a
-
-data Foo a = A a Int !a | B a | C ![[(Bar a)]] !(Bar a) ![[[(Bar a)]]]
-
-$(derive [makeFunctor, makeTraversable, makeFoldable,
-          smartConstructors,makeHaskellStrict]
-         [''Bar,''Foo])
--- src/Data/Comp/Derive/HaskellStrict.hs
+++ src/Data/Comp/Derive/HaskellStrict.hs
@@ -16,6 +16,7 @@ module Data.Comp.Derive.HaskellStrict
     (
      makeHaskellStrict
      , haskellStrict
+     , haskellStrict'
     ) where
 
 import Data.Comp.Derive.Utils
@@ -31,14 +32,19 @@ import Prelude hiding  (foldl, foldr,mapM, sequence)
 
 
 class HaskellStrict f where
-    thunkSequence' :: (Monad m) => f (TermT m g) -> m (f (TermT m g))
-    thunkSequence :: (Monad m, f :<: g) => f (TermT m g) -> TermT m g
-    thunkSequence t = thunk $ liftM inject $ thunkSequence' t
+    thunkSequence :: (Monad m) => f (TermT m g) -> m (f (TermT m g))
+    thunkSequenceInject :: (Monad m, f :<: g) => f (TermT m g) -> TermT m g
+    thunkSequenceInject t = thunk $ liftM inject $ thunkSequence t
+    thunkSequenceInject' :: (Monad m, f :<: g) => f (TermT m g) -> TermT m g
+    thunkSequenceInject' = thunkSequenceInject
 
 haskellStrict :: (Monad m, HaskellStrict f, f :<: g) => f (TermT m g) -> TermT m g
-haskellStrict = thunkSequence
+haskellStrict = thunkSequenceInject
 
-deepThunk d = iter d [|thunkSequence'|]
+haskellStrict' :: (Monad m, HaskellStrict f, f :<: g) => f (TermT m g) -> TermT m g
+haskellStrict' = thunkSequenceInject'
+
+deepThunk d = iter d [|thunkSequence|]
     where iter 0 _ = [|whnf'|]
           iter 1 e = e
           iter n e = iter (n-1) ([|mapM|] `appE` e)
@@ -55,15 +61,17 @@ makeHaskellStrict fname = do
   constrs_ <- P.mapM (liftM (isFarg fArg) . normalConStrExp) constrs
   if foldr (\ y x -> x && P.all null (snd y)) True constrs_
    then do
-     sequenceDecl' <- valD (varP 'thunkSequence') (normalB [|return|]) []
-     sequenceDecl <- valD (varP 'thunkSequence) (normalB [|inject|]) []
-     return [InstanceD [] classType [sequenceDecl', sequenceDecl]]
+     sequenceDecl <- valD (varP 'thunkSequence) (normalB [|return|]) []
+     injectDecl <- valD (varP 'thunkSequenceInject) (normalB [|inject|]) []
+     injectDecl' <- valD (varP 'thunkSequenceInject') (normalB [|inject|]) []
+     return [InstanceD [] classType [sequenceDecl, injectDecl, injectDecl']]
    else do
      constrs' <- P.mapM mkPatAndVars constrs_
-     sequenceDecl' <- funD 'thunkSequence' (map sequenceClause' constrs')
+     sequenceDecl <- funD 'thunkSequence (map sequenceClause' constrs')
      xn <- newName "x"
-     sequenceDecl <- funD 'thunkSequence [clause [varP xn] (normalB ([|thunk|] `appE` caseE (varE xn) (map sequenceMatch constrs'))) []] 
-     return [InstanceD [] classType [sequenceDecl', sequenceDecl]]
+     injectDecl <- funD 'thunkSequenceInject [clause [varP xn] (normalB ([|thunk|] `appE` caseE (varE xn) (map injectMatch constrs'))) []] 
+     injectDecl' <- funD 'thunkSequenceInject' (map injectClause' constrs')
+     return [InstanceD [] classType [sequenceDecl, injectDecl, injectDecl']]
       where isFarg fArg (constr, args) = (constr, map (containsStr fArg) args)
             containsStr _ (NotStrict,_) = []
             containsStr fArg (IsStrict,ty) = ty `containsType'` fArg
@@ -82,10 +90,57 @@ makeHaskellStrict fname = do
                        conBind (d, x) y = [| $(deepThunk d `appE` (varE x))  >>= $(lamE [varP x] y)|]
                    body <- P.foldr conBind [|return $conAp|] fvars
                    return $ Clause [pat] (NormalB body) []
-            sequenceMatch (con, pat,hasFargs,allVars, fvars) =
+            injectMatch (con, pat,hasFargs,allVars, fvars) =
                 do let conAp = P.foldl appE con allVars
                        conBind (d, x) y = [| $(deepThunk d `appE` (varE x))  >>= $(lamE [varP x] y)|]
                    body <- case fvars of
                              [] -> [|return (inject $conAp)|]
                              _ -> P.foldr conBind [|return (inject $conAp)|] fvars
                    return $ Match pat (NormalB body) []
+            injectClause' (con, pat,hasFargs,allVars, fvars) =
+                do let conAp = P.foldl appE con allVars
+                       conBind (d, x) y = [| $(deepThunk d `appE` (varE x))  >>= $(lamE [varP x] y)|]
+                   body <- case fvars of
+                             [] -> [|inject $conAp|]
+                             _ -> [| thunk |] `appE` P.foldr conBind [|return (inject $conAp)|] fvars
+                   return $ Clause [pat] (NormalB body) []
+
+
+-- {-| Derive an instance of 'Traversable' for a type constructor of any
+--   first-order kind taking at least one argument. -}
+-- makeHaskellStrict :: Name -> Q [Dec]
+-- makeHaskellStrict fname = do
+--   TyConI (DataD _cxt name args constrs _deriving) <- abstractNewtypeQ $ reify fname
+--   let fArg = VarT . tyVarBndrName $ last args
+--       argNames = map (VarT . tyVarBndrName) (init args)
+--       complType = foldl AppT (ConT name) argNames
+--       classType = AppT (ConT ''HaskellStrict) complType
+--   constrs_ <- P.mapM (liftM (isFarg fArg) . normalConStrExp) constrs
+--   if foldr (\ y x -> x && P.all null (snd y)) True constrs_
+--    then do
+--      sequenceDecl' <- valD (varP 'thunkSequence') (normalB [|return|]) []
+--      sequenceDecl <- valD (varP 'thunkSequence) (normalB [|inject|]) []
+--      return [InstanceD [] classType [sequenceDecl', sequenceDecl]]
+--    else do
+--      constrs' <- P.mapM mkPatAndVars constrs_
+--      sequenceDecl' <- funD 'thunkSequence' (map sequenceClause' constrs')
+--      sequenceDecl <- funD 'thunkSequence (map sequenceClause constrs')
+--      return [InstanceD [] classType [sequenceDecl', sequenceDecl]]
+--       where isFarg fArg (constr, args) = (constr, map (containsStr fArg) args)
+--             containsStr _ (NotStrict,_) = []
+--             containsStr fArg (IsStrict,ty) = ty `containsType'` fArg
+--             filterVar _ nonFarg [] x  = nonFarg x
+--             filterVar farg _ [depth] x = farg depth x
+--             filterVar _ _ _ _ = error "functor variable occurring twice in argument type"
+--             filterVars args varNs farg nonFarg = zipWith (filterVar farg nonFarg) args varNs
+--             mkCPat constr varNs = ConP constr $ map mkPat varNs
+--             mkPat = VarP
+--             mkPatAndVars (constr, args) =
+--                 do varNs <- newNames (length args) "x"
+--                    return (conE constr, mkCPat constr varNs,
+--                            any (not . null) args, map varE varNs, catMaybes $ filterVars args varNs (curry Just) (const Nothing))
+--             sequenceClause' (con, pat,hasFargs,allVars, fvars) =
+--                 do let conAp = P.foldl appE con allVars
+--                        conBind (d, x) y = [| $(deepThunk d `appE` (varE x))  >>= $(lamE [varP x] y)|]
+--                    body <- P.foldr conBind [|return $conAp|] fvars
+--                    return $ Clause [pat] (NormalB body) []
